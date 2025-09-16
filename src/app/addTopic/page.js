16ffd8135f4179baaa77803f67d56107b111eea4
Navneet Singh Gusain <gnavneet209@gmail.com>
2025-09-16 11:47:32 +0530

    Update page.js

--- src/app/addTopic/page.js
+++ src/app/addTopic/page.js
@@ -15,7 +15,7 @@ export default function AddTopic() {
       return;
     }
     try {
-      const res = await fetch("http://localhost:3000/api/topics",{
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ''}/api/topics`,{
         method: "POST",
         headers: {
           "Content-Type": "application/json",
